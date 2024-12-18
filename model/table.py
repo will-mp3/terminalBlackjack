7112b5b979f1349d5735db8d7ca3542cb490b496
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-18 18:41:54 -0500

    started Table class

---
+++ model/table.py
@@ -0,0 +1,21 @@
+from model import *
+
+class Table:
+
+    def __init__(self, deck):
+        self.deck = deck
+
+    def createTable():
+        print("--------------------")
+        print("|                  |")
+        print("|                  |")
+        print("|                  |")
+        print("|                  |")
+        print("|                  |")
+        print("|                  |")
+        print("|                  |")
+        print("--------------------")
+
+    
+
+    
