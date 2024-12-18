5bcdbb9b1de3fcfa7ffc35a0f051f0172924a51e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-18 18:41:34 -0500

    added new functionality for dealing to Deck class

--- model/deck.py
+++ model/deck.py
@@ -4,17 +4,34 @@ class Deck:
     SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     CARDS = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
-    def __init__(self, deckCount):
+    def __init__(self, deckCount = 2):
         self.deckCount = deckCount
         self.deck = [(card, suit) for card in Deck.CARDS for suit in Deck.SUITS] * self.deckCount
 
-    def cardVal(self):
-        if self.deck[0] in ['Jack', 'Queen', 'King']:
+    @classmethod
+    def cardVal(cls, card):
+        if card[0] in ['Jack', 'Queen', 'King']:
             return 10
-        elif self.deck[0] == 'Ace':
+        elif card[0] == 'Ace':
             return 11
         else:
-            return int(self.deck[0])
+            return int(card[0])
 
     def shuffleDeck(self):
         random.shuffle(self.deck)
+
+    def dealCard(self):
+        val = random.randint(0, 52 * self.deckCount)
+        card = self.deck[val]
+        return card
+
+    
+def main():
+    deck = Deck()
+    deck.shuffleDeck()
+    card = deck.dealCard()
+    print(card)
+    print(Deck.cardVal(card))
+
+if __name__ == "__main__":
+    main()
