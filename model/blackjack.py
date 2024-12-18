a15078fbf7dc196d865849cf3e0ec2d72ad7b2ed
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-18 18:41:49 -0500

    started Blackjack class

---
+++ model/blackjack.py
@@ -0,0 +1,12 @@
+from model import *
+
+class Blackjack:
+
+    def __init__(self, deck, table):
+        self.table = table
+        self.deck = deck
+
+    def run():
+        clear()
+        print("Welcome to the table! Please take a seat and place your bets.")
+        
