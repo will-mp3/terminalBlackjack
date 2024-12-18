59e1c28eff54abc5ed54a0541dbc0d4e5b44a4e3
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-18 15:18:54 -0500

    modified cardVal function

--- model/deck.py
+++ model/deck.py
@@ -8,13 +8,13 @@ class Deck:
         self.deckCount = deckCount
         self.deck = [(card, suit) for card in Deck.CARDS for suit in Deck.SUITS] * self.deckCount
 
-    def cardVal(self, card):
-        if card[0] in ['Jack', 'Queen', 'King']:
+    def cardVal(self):
+        if self.deck[0] in ['Jack', 'Queen', 'King']:
             return 10
-        elif card[0] == 'Ace':
+        elif self.deck[0] == 'Ace':
             return 11
         else:
-            return int(card[0])
+            return int(self.deck[0])
 
     def shuffleDeck(self):
         random.shuffle(self.deck)
