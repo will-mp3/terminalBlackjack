e64eb31ac30e96a46704470c9d9a2004f408346e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-19 19:12:45 -0500

    added initial blackjack game logic

--- model/blackjack.py
+++ model/blackjack.py
@@ -1,4 +1,4 @@
-from model import *
+from deck import Deck
 
 class Blackjack:
 
@@ -6,7 +6,53 @@ class Blackjack:
         self.table = table
         self.deck = deck
 
-    def run():
-        clear()
+    def run(self):
+        self.deck.shuffleDeck()
         print("Welcome to the table! Please take a seat and place your bets.")
-        
+        print()
+
+        # code to place bets
+        dealerHand, playerHand = [], []
+
+        # deal cards, in this order
+        playerHand.append(self.deck.dealCard())
+        dealerHand.append(self.deck.dealCard())
+        playerHand.append(self.deck.dealCard())
+        dealerHand.append(self.deck.dealCard())
+
+        print("The dealer is showing a", Deck.printCard(dealerHand[0]))
+        print()
+        print("You have", Deck.printCard(playerHand[0]), "and", Deck.printCard(playerHand[1]))
+        print()
+        playerVal = Deck.cardVal(playerHand[0]) + Deck.cardVal(playerHand[1])
+
+        # blackjack logic
+        if playerVal == 21:
+            print("Blackjack! You win!")
+            print()
+            # payout logic (1.5x)
+
+        # hit or stand logic
+        while 1:
+            move = input("Would you like to hit, stand, or double: (H/S/D)")
+            print()
+            if move == "H":
+                playerHand.append(self.deck.dealCard())
+                break
+            elif move == "S":
+                break
+            elif move == "D":
+                # double bet logic
+                playerHand.append(self.deck.dealCard())
+                break
+            else:
+                print("Invalid response, please respond again.")
+                print()
+
+def main():
+    deck = Deck()
+    game = Blackjack(deck, "NULL")
+    game.run()
+
+if __name__ == "__main__":
+    main() 
